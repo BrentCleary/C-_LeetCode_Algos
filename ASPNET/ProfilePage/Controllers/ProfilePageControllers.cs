08bf39cd3f221e7485482bc1f394bc473b6c021a
Brent Cleary <brent.cleary@gmail.com>
2023-08-14 18:19:42 -0700

    Css Update

--- ASPNET/ProfilePage/Controllers/ProfilePageControllers.cs
+++ ASPNET/ProfilePage/Controllers/ProfilePageControllers.cs
@@ -1,31 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 // Be sure to use your own project's namespace here! 
 namespace ProfilePage.Controllers;   
-public class HelloController : Controller   // Remember inheritance?    
+public class ProfilePageController : Controller   // Remember inheritance?    
 {      
-    [HttpGet] // We will go over this in more detail on the next page    
-    [Route("")] // We will go over this in more detail on the next page
+    [HttpGet("")] // We will go over this in more detail on the next page
     public ViewResult Index()        
     {            
-    	return View("Index");        
+      return View("Index");        
     }
 
-    [HttpGet("testing")]
-    public ViewResult Testing()
+    [HttpGet("projects")]
+    public ViewResult Projects()
     {
-      return View("TestTest");
+      return View("Projects");
     }
 
-  [HttpGet("numbers/{nums}")]
-  public string GetNumbers(string nums)
+  [HttpGet("contact")]
+  public ViewResult Contact()
   {
-    return $"These are the numbers: {nums}";
-  }
-
-  [HttpGet("lastpart/{finalstring}")]
-  public string FinalString(string finalstring)
-  {
-    return $"Last page for strings. Also, {finalstring}";
+    return View("Contact");
   }
 
 }
