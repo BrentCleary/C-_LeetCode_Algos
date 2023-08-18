c348fde2d96ca5478da2d577463878551ac8d12f
Brent Cleary <brent.cleary@gmail.com>
2023-08-17 22:41:09 -0700

    DojoModelsSurvey

---
+++ ASPNET/DojoSurveyModels/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyModels.Models;
+
+namespace DojoSurveyModels.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    // Index Page Survey
+
+    [HttpGet("/")]
+    public IActionResult Index()
+    {
+
+        return View();
+    }
+
+    // Post Request
+
+    [HttpPost("/process")]
+    public IActionResult Process(DojoModelsSurvey survey)
+    {
+
+        System.Console.WriteLine(survey.Name);
+        System.Console.WriteLine(survey.Location);
+        System.Console.WriteLine(survey.Language);
+        System.Console.WriteLine(survey.Message);
+
+        return View("Results", survey);
+    }
+
+
+    // Results Process
+
+    [HttpGet("/results")]
+    public IActionResult Results()
+    {
+
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ ASPNET/DojoSurveyModels/Models/DojoModelsSurvey.cs
@@ -0,0 +1,11 @@
+#pragma warning disable CS8618
+namespace DojoSurveyModels.Models;
+
+public class DojoModelsSurvey
+{
+  public string Name {get;set;}
+  public string Location {get;set;}
+  public string Language {get;set;}
+  public string Message {get;set;}
+  
+}
