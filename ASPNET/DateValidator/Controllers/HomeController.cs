fb3a152b5f85b52837b3ca3a5daed8bdd19ab7df
Brent Cleary <brent.cleary@gmail.com>
2023-08-19 09:41:48 -0700

    DateValidator

---
+++ ASPNET/DateValidator/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DateValidator.Models;
+
+namespace DateValidator.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("/submitdate")]
+    public IActionResult SubmitDate(CurrentDate currentdate)
+    {
+        return View("Result", currentdate);
+    }
+
+    [HttpGet("/dateresult")]
+    public IActionResult Result()
+    {
+        return View();
+    }
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ ASPNET/DateValidator/Models/Date.cs
@@ -0,0 +1,9 @@
+#pragma warning disable CS8618
+
+using System.ComponentModel.DataAnnotations;
+namespace DateValidator.Models;
+
+public class Date
+{
+  public DateTime CurrentDate {get;set;}
+}
