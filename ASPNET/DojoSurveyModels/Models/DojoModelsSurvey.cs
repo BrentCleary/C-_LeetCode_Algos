478ea52664a652120befb820ae2f8b4abf1c9f52
Brent Cleary <brent.cleary@gmail.com>
2023-08-19 08:52:34 -0700

    Validations

--- ASPNET/DojoSurveyModels/Models/DojoModelsSurvey.cs
+++ ASPNET/DojoSurveyModels/Models/DojoModelsSurvey.cs
@@ -1,11 +1,33 @@
 #pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
 namespace DojoSurveyModels.Models;
 
 public class DojoModelsSurvey
 {
+  [Required(ErrorMessage ="Name is Required")]
+  [MinLength(2, ErrorMessage ="Name must be at least 2 char")]
   public string Name {get;set;}
+  [Required(ErrorMessage = "Location is Required")]
+  [MinLength(2)]
   public string Location {get;set;}
+  [Required]
+  [MinLength(2)]
   public string Language {get;set;}
   public string Message {get;set;}
   
+}
+
+public class CustomValidations : ValidationAttribute
+{
+  protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+  {
+    if (LogicToBreakValidation)
+    {
+      return new ValidationResult("Custom error message");
+    }
+    else
+    {
+      return ValidationResult.Success;
+    }
+  }
 }
